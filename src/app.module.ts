6b36fb683a1d791f5f98bda5a9198be11b45b61e
adilet berdibekov <berdibekovadilet@gmail.com>
2022-10-24 15:57:42 +0600

    added get static to images/audio

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,16 @@
 import { Module } from "@nestjs/common";
 import { MongooseModule } from "@nestjs/mongoose";
+import { ServeStaticModule } from "@nestjs/serve-static";
 import { FileModule } from "./file/file.module";
 import { TrackModule } from "./track/track.module";
 import { UsersModule } from "./users/users.module";
+import { join } from "path";
 
 @Module({
   imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, "static"),
+    }),
     MongooseModule.forRoot(
       "mongodb+srv://admin:adi30013763@cluster0.ssnw1va.mongodb.net/music-app-server?retryWrites=true&w=majority"
     ),
