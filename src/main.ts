e22a15764ca4837ea8299da61c1e73b197d0d430
adilet berdibekov <berdibekovadilet@gmail.com>
2022-10-24 17:17:46 +0600

    eneble cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ async function start() {
   try {
     const PORT = process.env.PORT || 8000;
     const app = await NestFactory.create(AppModule);
+    app.enableCors();
     await app.listen(PORT, () =>
       console.log(`Start listening on port ${PORT}`)
     );
